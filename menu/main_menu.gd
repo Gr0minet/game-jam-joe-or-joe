525d37cae2a394772ac2e774bcd2a6af5362e83d
Gr0minet <not@existing.abcdef>
2024-12-01 17:29:42 +0100

    oof le pc y brule

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -9,6 +9,7 @@ const INTRO_TIME: float = 2.0
 @onready var buttons: VBoxContainer = $MarginContainer/Buttons
 @onready var how_to_play: TextureRect = $HowToPlay
 @onready var credits: TextureRect = $Credits
+@onready var margin_container_2: MarginContainer = $MarginContainer2
 
 var menu_background: CompressedTexture2D = preload("res://HUD/sprites/background.jpg")
 var story_background: CompressedTexture2D = preload("res://sprites/story_background.jpg")
@@ -20,7 +21,9 @@ func _ready() -> void:
 	background.texture = menu_background
 	background.modulate = Color.WHITE
 	buttons.modulate = Color.WHITE
+	margin_container_2.modulate = Color.WHITE
 	buttons.visible = true
+	margin_container_2.visible = true
 	AudioManager.play_music(SoundBank.main_menu_music)
 
 
@@ -44,8 +47,10 @@ func _on_jouer_button_pressed() -> void:
 	var tween: Tween = get_tree().create_tween()
 	tween.tween_property(background, "modulate", Color.BLACK, TRANSITION_TIMES[0])
 	tween.parallel().tween_property(buttons, "modulate", Color.BLACK, TRANSITION_TIMES[0])
+	tween.parallel().tween_property(margin_container_2, "modulate", Color.BLACK, TRANSITION_TIMES[0])
 	await tween.finished
 	buttons.visible = false
+	margin_container_2.visible = false
 	AudioManager.play_music(SoundBank.intro_music)
 	await get_tree().create_timer(TRANSITION_TIMES[1]).timeout
 	background.texture = story_background
